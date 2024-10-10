8882e6261cd89831016a1bb69ae2e857206f7fec
PandaTech <pandatech1999@gmail.com>
2024-10-10 11:49:13 +0300

    Testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ParentComponent } from "./parent/parent.component";
+import { ChildComponent } from './child/child.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ParentComponent, ChildComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
-  title = 'my-angular-app';
+  title: string = "Hello world"
+  btn: string = "Press here"
+  isDisabled: boolean = true;
+  ImgAngular: string = '../assets/computer.jpg'
+
+  //style binding 
+  bgColor: string = "crimson";
+  TextColor: string = "black"
+  DcColor: string = "cyan"
+
+  //class binding
+  redText:string = 'abcd'
 }
---
+++ src/app/child/child.component.ts
@@ -0,0 +1,17 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  standalone: true,
+  imports: [],
+  templateUrl: './child.component.html',
+  styleUrl: './child.component.scss',
+  styles: `.btn { padding: 5px; }`,
+})
+export class ChildComponent {
+  @Output() addItemEvent = new EventEmitter<string>();
+
+  addItem() {
+    this.addItemEvent.emit('🐢');
+  }
+}
